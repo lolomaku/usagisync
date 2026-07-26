bf1b3c44cabf34785974c32e768a0ff99ec7308a
Lolo Maku <markpajao@outlook.com>
2026-07-26 20:02:34 +0800

    Update Migration.kt

--- src/main/kotlin/org/kotatsu/plugins/Migration.kt
+++ src/main/kotlin/org/kotatsu/plugins/Migration.kt
@@ -4,16 +4,18 @@ import io.ktor.server.application.*
 import org.flywaydb.core.Flyway
 
 fun Application.runMigrations() {
-    val dbConfig = environment.config.config("database")
+    val name = environment.config.property("database.name").getString()
+    val host = environment.config.property("database.host").getString()
+    val port = environment.config.property("database.port").getString()
+    val dialect = environment.config.property("database.dialect").getString()
+    val user = environment.config.property("database.user").getString()
+    val password = environment.config.property("database.password").getString()
 
-    val host = dbConfig.property("host").getString()
-    val port = dbConfig.property("port").getString()
-    val dbName = dbConfig.property("name").getString()
-    val user = dbConfig.property("user").getString()
-    val password = dbConfig.property("password").getString()
+    // Add SSL parameters explicitly here for Flyway
+    val url = "jdbc:$dialect://$host:$port/$name?sslMode=REQUIRED&useSSL=true&allowPublicKeyRetrieval=true"
 
     val flyway = Flyway.configure()
-        .dataSource("jdbc:mysql://$host:$port/$dbName", user, password)
+        .dataSource(url, user, password)
         .load()
 
     flyway.migrate()
