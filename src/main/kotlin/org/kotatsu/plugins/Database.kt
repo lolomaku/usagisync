4c080749b9e9bcd6e4f8b832f04a4db40041a7cc
Lolo Maku <markpajao@outlook.com>
2026-07-26 19:15:01 +0800

    Update Database.kt

--- src/main/kotlin/org/kotatsu/plugins/Database.kt
+++ src/main/kotlin/org/kotatsu/plugins/Database.kt
@@ -13,17 +13,25 @@ fun Application.configureDatabase() {
     val port = environment.config.property("database.port").getString()
     val dialect = environment.config.property("database.dialect").getString()
 
-    val jdbcDriver = when (dialect.lowercase()) {
-        "mariadb" -> "org.mariadb.jdbc.Driver"
-        "mysql" -> "com.mysql.cj.jdbc.Driver"
-        else -> error("Unsupported DATABASE_DIALECT: $dialect")
+    val isMaria = dialect.lowercase() == "mariadb"
+    val jdbcDriver = if (isMaria) "org.mariadb.jdbc.Driver" else "com.mysql.cj.jdbc.Driver"
+
+    // MariaDB driver and MySQL driver expect slightly different SSL query params
+    val sslParams = if (isMaria) {
+        "?useSSL=true&trustServerCertificate=true"
+    } else {
+        "?sslMode=REQUIRED&useSSL=true&allowPublicKeyRetrieval=true"
     }
 
     val config = HikariConfig().apply {
-        jdbcUrl = "jdbc:$dialect://$host:$port/$name?sslMode=REQUIRED"
+        jdbcUrl = "jdbc:$dialect://$host:$port/$name$sslParams"
         username = environment.config.property("database.user").getString()
         password = environment.config.property("database.password").getString()
         driverClassName = jdbcDriver
+        
+        // Explicit properties passed to Hikari
+        addDataSourceProperty("useSSL", "true")
+        addDataSourceProperty("trustServerCertificate", "true")
     }
 
     hikariDataSource = HikariDataSource(config)
